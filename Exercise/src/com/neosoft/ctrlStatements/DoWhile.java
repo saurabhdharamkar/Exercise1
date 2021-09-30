d7e76733efbb6ba8fbb7afc74b3a73731f8c0ebd
saurabhdharamkar <saurabhdharamkar2714@gmail.com>
2021-09-30 10:11:53 -0700

    Commit

---
+++ Exercise/src/com/neosoft/ctrlStatements/DoWhile.java
@@ -0,0 +1,15 @@
+package com.neosoft.ctrlStatements;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		  int i = 0;
+		    do {
+		      System.out.println(i);
+		      i++;
+		    }
+		    while (i < 5);  
+
+	}
+
+}
---
+++ Exercise/src/com/neosoft/ctrlStatements/ForLoop.java
@@ -0,0 +1,13 @@
+package com.neosoft.ctrlStatements;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		int no=15;
+		for(int i=1;i<=15;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
