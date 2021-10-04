bc8dafa6497b60995e01b825cc172dd81d85e2b0
saurabhdharamkar <saurabhdharamkar2714@gmail.com>
2021-10-04 09:50:37 -0700

    Commit

---
+++ Exercise/src/com/neosoft/cmdLineExample/CmdLineExample.java
@@ -0,0 +1,10 @@
+package com.neosoft.cmdLineExample;
+
+public class CmdLineExample {
+
+	public static void main(String[] args) {
+		for(int i=0;i<args.length;i++)  
+			System.out.println(args[i]);  
+	}
+
+}
---
+++ Exercise/src/com/neosoft/cmdLineExample/CommandLine.java
@@ -0,0 +1,10 @@
+package com.neosoft.cmdLineExample;
+
+class CommandLine
+{
+    public static void main(String args[ ])
+    {
+        String name = args[3];
+        System.out.println("The name of the user is: " +name);
+    }
+}
