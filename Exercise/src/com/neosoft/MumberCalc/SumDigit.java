d96e3d25a9efcf5151aa1ffc3c0f373fa1088560
saurabhdharamkar <saurabhdharamkar2714@gmail.com>
2021-10-05 10:33:21 -0700

    Commit

---
+++ Exercise/src/com/neosoft/MumberCalc/SumDigit.java
@@ -0,0 +1,15 @@
+package com.neosoft.MumberCalc;
+
+public class SumDigit {
+
+	public static void main(String[] args) {
+		
+		int a=20;
+		int b=30;
+		
+		int sum=a+b;
+		System.out.println("Sum :"+sum);
+		
+	}
+
+}
