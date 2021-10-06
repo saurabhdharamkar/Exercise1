ae1228453c0d8d2ef2542a7649a96d7443853e76
saurabhdharamkar <saurabhdharamkar2714@gmail.com>
2021-10-06 10:38:17 -0700

    Commit

---
+++ Exercise/src/com/neosoft/roomClass/CallByValue.java
@@ -0,0 +1,16 @@
+package com.neosoft.roomClass;
+
+public class CallByValue { 
+	
+	
+	void callIt(int data) {
+		System.out.println("Your Data is :"+data);
+	}
+
+	public static void main(String[] args) {
+		CallByValue cb=new CallByValue();
+		cb.callIt(500);
+
+	}
+
+}
---
+++ Exercise/src/com/neosoft/roomClass/SimpleObject.java
@@ -0,0 +1,15 @@
+package com.neosoft.roomClass;
+
+public class SimpleObject {
+	
+	
+	public SimpleObject() {
+		System.out.println("Hello This Constrctor Program");
+	}
+
+	public static void main(String[] args) {
+		
+		SimpleObject ob=new SimpleObject();
+	}
+
+}
